001cf945ca1a995bb116269f26fe2bead9cc1c56
Harmony2 <34325108+Harmony2@users.noreply.github.com>
2018-10-11 13:23:57 -0300

    v1

---
+++ liga.py
@@ -0,0 +1,47 @@
+import requests
+import re
+import sys
+
+total = 0
+ind = []
+notf = []
+#def getpage(card):
+
+
+def getprice(card):
+	global notf
+	global total
+	global ind
+	card = card.replace(' ','+')
+	r = requests.get("https://ligamagic.com.br/?view=cards%"+"2Fsearch&card=%s"%card)
+	s = r.text
+	price = re.search(r"precos-menor'>R\$ (.*?) <", s)
+	try:
+		p = price[1]
+		p = p.replace(',','.')
+		total += float(p)
+		ind.append([card,p])
+		return(p)
+	except:
+		notf.append(card)
+
+def getlist(arq):
+	f = open(arq,'r')
+	s = f.read()
+	f.close()
+	s = s.split('\n')
+	return s
+
+lista = getlist(sys.argv[1])
+c = 1
+for i in lista:
+	print('%s/%s'%(c,len(lista)))
+	getprice(i)
+	c+=1
+print('total price: R$:%s'%total)
+
+for i in ind:
+	print(i[0], i[1])
+print('Not found:')
+for i in notf:
+	print(i)
